9879a7c3185341469d0b5818b6ac4ce76040140e
Cody Evol <cody@evolcreative.com>
2015-10-29 09:54:00 -0700

    Step 2

---
+++ CarouselDemo/IntroViewController.swift
@@ -0,0 +1,35 @@
+//
+//  IntroViewController.swift
+//  CarouselDemo
+//
+//  Created by Cody Evol on 10/29/15.
+//  Copyright © 2015 Cody Evol. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
