18f35391a309329edbf8ed6276e279750d14d6a1
Cody Evol <cody@evolcreative.com>
2015-11-02 19:03:06 -0800

    Step 9 Stable

---
+++ CarouselDemo/Converstations.swift
@@ -0,0 +1,40 @@
+//
+//  Converstations.swift
+//  CarouselDemo
+//
+//  Created by Cody Evol on 11/2/15.
+//  Copyright © 2015 Cody Evol. All rights reserved.
+//
+
+import UIKit
+
+class Converstations: UIViewController {
+
+    @IBOutlet weak var convoBack: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func convoBack(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+
+}
